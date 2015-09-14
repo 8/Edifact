a3ab4db96c27203b97dda9df64ff8fb088b002a5
mdk <mdk@lostindetails.com>
2015-09-14 18:44:34 +0200

    added name to AssemblyInfo

--- Edifact/Properties/AssemblyInfo.cs
+++ Edifact/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Edifact")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © martin kramer 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
